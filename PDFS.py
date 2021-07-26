b8345f6a061c6e6c6a3e7ca3908bf53a214fcccc
Lstew-bit <3288415049@qq.com>
2021-07-26 22:19:39 +0800

    Changed the input, namely the graph for presentation.

--- PDFS.py
+++ PDFS.py
@@ -59,13 +59,23 @@ class Graph:
 g = Graph(5)
 g.addEdge(0, 1)
 g.addEdge(0, 2)
+g.addEdge(0, 3)
 g.addEdge(0, 4)
+g.addEdge(1, 0)
 g.addEdge(1, 2)
+g.addEdge(1, 4)
+g.addEdge(2, 1)
+g.addEdge(2, 3)
 g.addEdge(2, 4)
+g.addEdge(3, 0)
+g.addEdge(3, 2)
+g.addEdge(3, 4)
+g.addEdge(4, 0)
+g.addEdge(4, 1)
+g.addEdge(4, 2)
 g.addEdge(4, 3)
-
 s = 0
-d = 3
+d = 4
 
 print("Followings are different paths from %d to %d :" %(s, d))
 g.printAllPaths(s, d)
