0776e04e5e97f3d2db0a56424c1fecdc72e3ca85
Lstew-bit <3288415049@qq.com>
2021-07-30 23:46:20 +0800

    Changed fundemental way to produce comparison matrix.

--- AHP.py
+++ AHP.py
@@ -26,116 +26,77 @@ allPaths = {    'path_1' : { "time" : 2,
                              "height" : 0.29,
                              "distance" : 1}
             }
+time_comparisons = {}
+height_comparisons = {}
+distance_comparisons = {}
 
-''' function to generate comparison matrix for each factor
-    creat 3 different matrix
-    everytime it sorts elements by one of 3 factors
+# function used to generate comparison matrix
+def make_cmp(dictionary, cmp_obj):
+    for key, value in dictionary.items():
+        for key2, value2 in dictionary.items():
+            if value > value2:
+                dif = (value - value2) / value2 * 10
+                dif = round(dif)
+                if(cmp_obj == 'time'):
+                    time_comparisons[key, key2] = [dif]
+                if(cmp_obj == 'height'):
+                    height_comparisons[key, key2] = [dif]
+                if(cmp_obj == 'distance'):
+                    distance_comparisons[key, key2] = [dif]
+
+            else:
+                dif = (value2 - value) / value * 10
+                dif = round(dif)
+                if(cmp_obj == 'time'):
+                    time_comparisons[key, key2] = [dif]
+                if(cmp_obj == 'height'):
+                    height_comparisons[key, key2] = [dif]
+                if(cmp_obj == 'distance'):
+                    distance_comparisons[key, key2] = [dif]
+
+
 '''
-# sorted by time from shortest to greatest
-# print('Below are the sorted paths from shortest time to greatest: ')
-# sortedPathByTime = sorted(allPaths, key=lambda x: (allPaths[x]['time']))
-# print(sortedPathByTime)
-# print('\n')
-#
-# # sorted by height from shortest to greatest
-# print('Below are the sorted paths from shortest height to greatest: ')
-# sortedPathByHeight = sorted(allPaths, key=lambda x: (allPaths[x]['height']))
-# print(sortedPathByHeight)
-# print('\n')
-#
-#
-# # sorted by distance from shortest to greatest
-# print('Below are the sorted paths from shortest distance to greatest: ')
-# sortedPathByDistance = sorted(allPaths, key=lambda x: (allPaths[x]['distance']))
-# print(sortedPathByDistance)
-# print('\n')
+    do a pairwise comparison for allPaths this dictionary for each criterion
+    rule: set the smaller one as the base, namely 1, then calculate how greater
+    the proportion is the bigger one compared to the smaller one and multiply it
+    by 10 and round it to the neartest integer so it's between 1 and 9. If they are equal
+    then both of them are 1 in Satty scale.
 
 '''
-    calculate relative strength for each candidate against each criterion
-    for each criterion, add up all paths' values to get the sum which then divides relative strength for one particular path
+all_time = {}
+all_height = {}
+all_distance = {}
 
-'''
-
-# for time, height, distance
-# get the value of time for each path --> get the sum of them --> divide each path's time by the
-sum_time = 0
-sum_height = 0
-sum_distance = 0
+# store all paths' time
 for key, value in allPaths.items():
-    sum_time += value['time']
-    sum_height += value['height']
-    sum_distance += value['distance']
+    for key2, value2 in value.items():
+        if key2 == 'time':
+            all_time[key] = value2
+print('Time for each path: ' )
+print(all_time)
 
-#check correctness of summation. Succeed
-print('sum of time, height, distance, respectively: ')
-print(sum_time, sum_height, sum_distance)
+# store all paths' heights
+for key, value in allPaths.items():
+    for key2, value2 in value.items():
+        if key2 == 'height':
+            all_height[key] = value2
+print('Height for each path: ' )
+print(all_height)
+
+# store all paths' distance
+for key, value in allPaths.items():
+    for key2, value2 in value.items():
+        if key2 == 'distance':
+            all_distance[key] = value2
+print('Distance for each path: ' )
+print(all_distance)
 print('\n')
 
-'''
-    calculate relative strength (the result is rounded for the sake of integer rule of Satty scale)
-    if the result is 0.0, then all the other result is added by 1
-    if the result is greater than 9, then this result is set to 9
-'''
-rs_time = {}
+# make comparisons for three criteria
+make_cmp(all_time, 'time')
+make_cmp(all_height, 'height')
+make_cmp(all_distance, 'distance')
 
-for key, value in allPaths.items():
-    rs = value['time'] / sum_time * 10
-    rs_time[key] = round(rs)
-
-out_of_bounds = False
-for key, value in rs_time.items():
-    if out_of_bounds == True:
-        break
-    if value == 0.0:
-        for key, value in rs_time.items():
-            rs_time[key] += 1
-            out_of_bounds = True
-    if value > 9.0:
-        rs_time[key] = 9.0
-
-print('path relative strength against time: ')
-print(rs_time)
-print('\n')
-
-
-'''
-    calculat relative strength against height
-'''
-rs_height = {}
-
-for key, value in allPaths.items():
-    rs = value['height'] / sum_height * 10
-    rs_height[key] = round(rs)
-
-out_of_bounds = False
-for key, value in rs_height.items():
-    if out_of_bounds == True:
-        break
-    if value == 0.0:
-        for key, value in rs_height.items():
-            rs_height[key] += 1
-            out_of_bounds = True
-    if value > 9.0:
-        rs_height[key] = 9.0
-
-print('path relative strength against height: ')
-print(rs_height)
-print('\n')
-
-'''
-    calculat relative strength against distance
-    notice that the relative strength against distance is reversed, meaning
-    the shortest path has highest relative strength, as opposed to the case
-    in previous two.
-'''
-rs_distance = {}
-
-for key, value in allPaths.items():
-    rs = value['distance'] / sum_distance * 10
-    rs_distance[key] = round(rs)
-
-
-
-print('path relative strength against distance: ')
-print(rs_distance)
-print('\n')
+print(time_comparisons)
+print(height_comparisons)
+print(distance_comparisons)
